25981daf04e4a3439426aab61938a33cc70ec3d5
Michael Bonifacio <michaelbonifacio@gmail.com>
2016-11-19 10:23:51 -0800

    Clean up comments

--- performTrainingDataExtraction.m
+++ performTrainingDataExtraction.m
@@ -4,19 +4,19 @@ winSize = 11;
 nbSamplPerCaseMax = 100;
 
 rootCSV = '../Data/t2/';
-destDir = '../Data/t2/masks/';
+maskDir = '../Data/t2/masks/';
 destDirPatches = '../Data/t2/';
 
 listMasksName = dir(strcat(destDir, '*.mat'));
 for i=1:length(listMasksName)
     fnameMask = strcat(destDir, listMasksName(i).name);
-    
+
     % read mask
     load(fnameMask, 'mask', 'uslices');
     data{i}.mask = mask;
     data{i}.slices = uslices;
-    
-    % read dicom    
+
+    % read dicom
     listDicom = subdir(strcat(rootCSV, num2str(i), '/*.dcm'));
     for j=1:numel(uslices)
         fname = listDicom(uslices(j)).name;
@@ -29,65 +29,44 @@ X1 = {};
 % extract patches
 for i=1:length(data)
     for slice=1:numel(data{i}.slices)
-        
-        tmp = data{i}.mask{slice};              
+
+        tmp = data{i}.mask{slice};
         [pty, ptx] = find(tmp > 0);
-        
+
         Ys = zeros(1,numel(pty));
         for j=1:numel(pty)
             Ys(j) = tmp(pty(j), ptx(j)) - 1;
         end
-        
+
         Ys = logical(Ys);
         infarctedPty = pty(Ys);
         infarctedPtx = ptx(Ys);
-        
+
         normalPty = pty(~Ys);
         normalPtx = ptx(~Ys);
-        
+
         nbSamplPerCase = min(nnz(Ys), nbSamplPerCaseMax);
-        
+
         rand('seed', 0);
         indxInfa = randperm(length(infarctedPty), nbSamplPerCase);
 
         rand('seed', 0);
         indxNorm = randperm(length(normalPty), nbSamplPerCase);
-        
+
         Y2 = [ones(1,nbSamplPerCase,1) zeros(1,nbSamplPerCase,1)];
-        
+
         pty2 = [infarctedPty(indxInfa(1:nbSamplPerCase))' normalPty(indxNorm(1:(nbSamplPerCase)))' ]';
         ptx2 = [infarctedPtx(indxInfa(1:nbSamplPerCase))' normalPtx(indxNorm(1:(nbSamplPerCase)))' ]';
-        
-        
+
         im1 = squeeze(data{i}.X{slice});
         im1(im1<0) = 0;
-        im1 = im1 ./ max(im1(:));        
-        
-%         imagesc(im1); hold on; 
-%         line([1   size(im1,2)], [size(im1,1) /2 size(im1,1) /2], 'Color', 'white'); 
-%         hold on;
-%         colormap(gray);
-%         
-%         for j=1:length(pty2)
-%             if(labels{patient}(pty2(j),ptx2(j),slice))
-%                 plot( ptx2(j), pty2(j), 'wo');
-%             else
-%                 plot( ptx2(j), pty2(j), 'ro');
-%             end
-%         end
-%             
-%         close;
-        
+        im1 = im1 ./ max(im1(:));
+
         cb1 = extractCuboidNoReshape(im1,  pty2, ptx2, winSize);
-      %  cb2 = extractCuboidNoReshape(flip(im1,1), pty2, ptx2, winSize);
-        
-   
+
         X1{i} = cat(3, X1{i}, cb1);
-%        X2{patient} = cat(3, X2{patient}, cb2);
-       
- %       X{patient} = cb + 1;       
         Y{patient} = Y2;
-        
+
         clear cb1 cb2 cb Y2 tmp1 tmp2 im1 pty2 ptx2 Ys;
         break;
     end
