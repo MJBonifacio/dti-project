13e615cb559839992e0155c0faa66a3a688f1bee
Michael Bonifacio <michaelbonifacio@gmail.com>
2016-11-20 14:30:52 -0800

    Write function to load filenames of all infarcted slices

--- code/transformations/computeBrainTransformationADC.m
+++ code/transformations/computeBrainTransformationADC.m
@@ -5,21 +5,20 @@ function computeBrainTransformationADC()
 %   2) # of pixels of translation necessary to maximize vertical correlation
 
 %path constants
-    rootData = './data';
-    maskDir = './data/masks';
-    destDir = './transformationsADC'; 
-
+    rootData = '../../mat_data';
+    maskDir = '../../masks';
+    destDir = './transformationsADC';
 
 %load data
     listDirs = dir(rootData);
     numCases = size(listDirs,1);
-    
+
 for i = 1:numCases %numloops == numstudies
-    
+
     if ~listDirs(i).isdir || strcmp(listDirs(i).name,'.') || strcmp(listDirs(i).name,'..') || strcmp(listDirs(i).name,'masks') || strcmp(listDirs(i).name,'transformationsADC') || strcmp(listDirs(i).name,'transformationsDTI')
         continue
     end
-    
+
     studyNumber = listDirs(i).name;
     tempVars = fullfile(maskDir,strcat(studyNumber,'.mat'));
     load(tempVars); %mask and uslices
@@ -31,12 +30,12 @@ for i = 1:numCases %numloops == numstudies
     %used to determine amount of money to preallocate
     tmpADCfname = fullfile(pathADC,listADC(1).name);
     tmpADCinfo = dicominfo(tmpADCfname);
-        
+
     sizeADC = size(uint8(dicomread(tmpADCfname)));
     listImagesADC = cell(numel(uslices),1);
     listInfoADC = repmat(tmpADCinfo,numel(uslices),1);
     listSliceTransformationsADC = zeros(numel(uslices),2,1);
-        
+
     for j = 1:numel(uslices)
         fnameSlice = fullfile(pathADC,listADC(uslices(j)).name);
         listImagesADC{j} = uint8(dicomread(fnameSlice));
@@ -46,18 +45,18 @@ for i = 1:numCases %numloops == numstudies
         listSliceTransformationsADC(j,1,1) = tmpAngle;
         listSliceTransformationsADC(j,2,1) = tmpTrans;
     end
-    
+
     fnameTransformation = fullfile(destDir,strcat(studyNumber,'.mat'));
     save(fnameTransformation,'listInfoADC','listSliceTransformationsADC','sizeADC');
 
 %    pathDTI = fullfile(rootData,studyNumber,'DTI');
 %    listDTI = dir(fullfile(pathDTI,'*.dcm'));
-    
+
 %load DTI for this study for reference/comparisons
     %preallocate cell/struct arrays
     %    tmpDTIfname = fullfile(pathDTI,listDTI(1).name);
     %    tmpDTIinfo = dicominfo(tmpDTIfname);
-        
+
     %    numAllDTI = size(listDTI,1);
     %    sizeDTI = size(dicomread(tmpDTIfname));
     %    allImagesDTI = cell(numAllDTI, 1);
@@ -68,9 +67,9 @@ for i = 1:numCases %numloops == numstudies
     %        fnameSlice = fullfile(pathDTI,listDTI(j).name);
     %        allImagesDTI{j} = dicomread(fnameSlice);
     %        allInfoDTI(j) = dicominfo(fnameSlice);
-    %    end          
+    %    end
 
-    
+
     %compute DTI slices by closest slice location
         %listSlicesDTI = cell(numel(uslices),1);
         %for j = 1:numel(uslices)
