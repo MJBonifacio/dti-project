21f4b0b0fac5d7c145abf9aaa4b36b14f1729d6b
Michael Bonifacio <michaelbonifacio@gmail.com>
2016-11-21 17:54:23 -0800

    Rename folder to contralateral-dti

--- code/contralateral-adc/extractCuboid.m
+++
@@ -1,7 +0,0 @@
-function [win] = extractCuboidNoReshape(img, pty, ptx, winSize)
-x = floor(winSize/2);
-win = zeros(winSize, winSize, numel(pty), 'single');
-
-for i=1:numel(pty)
-  win(:,:,i) = img(pty(i)-x:pty(i)+x, ptx(i)-x:ptx(i)+x);
-end
