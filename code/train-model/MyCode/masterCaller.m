b28a7743fb419d86eb0db013df73f01bf878967c
Michael Bonifacio <michaelbonifacio@gmail.com>
2016-11-27 08:53:16 -0800

    Reduce patches per trainingcase

--- code/train-model/MyCode/masterCaller.m
+++ code/train-model/MyCode/masterCaller.m
@@ -3,6 +3,7 @@ function masterCaller()
 dirnameRoc = '../roc';
 dirnameSR = '../SR';
 fnameTrainingSet = '../vectorizedTrainingSet.mat';
+patchesToTrain = 3000;
 
 addpath(dirnameRoc,dirnameSR);
 load(fnameTrainingSet, 'patchOffsets', 'patchCounts', 'setX', 'setY');
@@ -15,9 +16,37 @@ Ytruth = [];
 
 %leave-one-out cross-validation
 for j = 1 : numPatients
+    j
     [Xtest, localYtruth, Xtrain, Ytrain] = leaveOneOut(randIndex(j), patchOffsets, patchCounts, setX, setY);
 
+    beginPatch = 1;
+    endPatch = patchesToTrain;
+    numPatches = length(Xtrain);
+
+    Yhat = [];
+
     %Produce Yhat vector for ROC evaluation
+    while endPatch < numPatches
+      beginPatch
+      endPatch
+      numPatches
+      % localXtest = Xtest(beginPatch:endPatch);
+      localXtrain = Xtrain(beginPatch:endPatch);
+      localYtrain = Ytrain(beginPatch:endPatch);
+      % Yhat = [Yhat; callKSR(localXtest, localXtrain, localYtrain)];
+      Yhat = [Yhat; callKSR(Xtest, localXtrain, localYtrain)];
+
+      beginPatch = endPatch + 1;
+      endPatch = endPatch + 1 + patchesToTrain;
+    end
+
+    endPatch = numPatches;
+    % localXtest = Xtest(beginPatch:endPatch);
+    localXtrain = Xtrain(beginPatch:endPatch);
+    localYtrain = Ytrain(beginPatch:endPatch);
+    % Yhat = [Yhat; callKSR(localXtest, localXtrain, localYtrain)];
+    Yhat = [Yhat; callKSR(Xtest, localXtrain, localYtrain)];
+
     Yhat = callKSR(Xtest, Xtrain, Ytrain);
     totalYhat = [totalYhat; Yhat];
     Ytruth = [Ytruth; localYtruth];
